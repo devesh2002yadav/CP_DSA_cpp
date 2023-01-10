2912f9bb0f6076ba4d6cf924b201d956fcf0f9b1
devesh2002yadav <77203935+devesh2002yadav@users.noreply.github.com>
2023-01-10 22:39:26 +0530

    Add files via upload

---
+++ GS leetcode min consecutive cards to pick.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCardPickup(vector<int>& cards) 
+    {
+        int ans=1000000;
+        int n=cards.size();
+        unordered_map<int, int> dm;
+        for(int i=0; i<n; i++)
+        {
+            if(dm.find(cards[i])!=dm.end())
+            {
+                ans=min(ans, i-dm[cards[i]]+1);
+            }
+            dm[cards[i]]=i;
+        }
+        if(ans==1000000) return -1;
+        else return ans;
+    }
+};
